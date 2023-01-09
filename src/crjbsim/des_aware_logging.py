922e4effef8084c24871d49875fb014376a65913
Chris Brett <chrisbrett665@gmail.com>
2023-01-09 15:45:27 +0000

    refactor des aware log handler to mixin for use with any handler subclass

--- src/crjbsim/des_aware_logging.py
+++ src/crjbsim/des_aware_logging.py
@@ -1,17 +1,25 @@
 import logging
-from logging import StreamHandler
+from logging import FileHandler, Handler, StreamHandler
 
 from crjbsim import time_provider
 
 
-class Handler(StreamHandler):
+class DESAwareHandlerMixin(Handler):
     def format(self, record):
         return f"{time_provider.get_time_formatted()} {super().format(record)}"
 
 
+class DESAwareStreamHandler(StreamHandler, DESAwareHandlerMixin):
+    pass
+
+
+class DESAwareFileHandler(FileHandler, DESAwareHandlerMixin):
+    pass
+
+
 def setup():
     root_logger = logging.getLogger()
-    handler = Handler()
+    handler = DESAwareStreamHandler()
     handler.formatter = logging.Formatter('%(levelname)-8s: %(message)s (%(name)s)')
     root_logger.handlers = [handler]
     root_logger.setLevel(logging.DEBUG)
