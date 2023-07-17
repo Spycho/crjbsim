b4bf9532cd27a216e32121b481e29ee10a5af679
Chris Brett <chrisbrett665@gmail.com>
2023-07-17 11:31:23 +0100

    change time_provider to be in seconds, to match asyncio.sleep call

--- src/crjbsim/time_provider.py
+++ src/crjbsim/time_provider.py
@@ -3,21 +3,21 @@ import datetime
 
 class _TimeProvider:
     def __init__(self):
-        self.time = 0  # ms
+        self.time = 0  # seconds
 
 
 _instance = _TimeProvider()
 
 
 def get_time() -> int:
-    """Gets the system time in milliseconds"""
+    """Gets the system time in seconds"""
     return _instance.time
 
 
 def get_time_formatted() -> str:
-    return datetime.datetime.fromtimestamp(_instance.time / 1000).strftime('%H:%M:%S.%f')[:-3]
+    return datetime.datetime.fromtimestamp(_instance.time).strftime('%H:%M:%S.%f')[:-3]
 
 
 def set_time(time: int):
-    """Sets the system time in milliseconds. Should only be called from the discrete event scheduler"""
+    """Sets the system time in seconds. Should only be called from the discrete event scheduler"""
     _instance.time = time
